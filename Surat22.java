3684e84a14af1c07bf5d5b296e9b615d8c21bb5c
Yann <yann@gmail.com>
2026-04-28 08:15:46 +0700

    Tugas Jobsheet 9 class Surat22

---
+++ Surat22.java
@@ -0,0 +1,27 @@
+public class Surat22 {
+    String idSurat;
+    String namaMahasiswa;
+    String kelas;
+    char jenisIzin;
+    int durasi;
+
+    public Surat22() {
+    }
+
+    public Surat22(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas = kelas;
+        this.jenisIzin = jenisIzin;
+        this.durasi = durasi;
+    }
+
+    public void tampilkanData() {
+        System.out.println("ID Surat       : " + idSurat);
+        System.out.println("Nama Mahasiswa : " + namaMahasiswa);
+        System.out.println("Kelas          : " + kelas);
+        System.out.println("Jenis Izin     : " + jenisIzin);
+        System.out.println("Durasi         : " + durasi + " hari");
+        System.out.println("--------------------------");
+    }
+}
